7d9cbc54a76e36f96661e7a2d149b0c393431d62
Custler <svt@zmail.ru>
2021-12-04 20:37:10 +0300

    fix balance_check.sh

--- scripts/balance_check.sh
+++ scripts/balance_check.sh
@@ -22,6 +22,7 @@
 SCRIPT_DIR=`cd "$(dirname "${BASH_SOURCE[0]}")" && pwd -P`
 export NODE_TOP_DIR=$(cd "${SCRIPT_DIR}/../" && pwd -P)
 
+NODE_BIN_DIR="$HOME/bin"
 KEYS_DIR="${NODE_TOP_DIR}/ton-keys"
 Contr_path="${NODE_TOP_DIR}/contracts"
 
